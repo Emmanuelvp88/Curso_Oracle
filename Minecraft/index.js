24ce2fe7da71da2c8818b076b1ed7ad9944a213e
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-23 21:58:32 -0600

    Agregamos los comentearios

--- Minecraft/index.js
+++ Minecraft/index.js
@@ -3,19 +3,11 @@ var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 
 pincel.fillStyle = "green"
-pincel.fillRect(50, 50, 350, 300);
+pincel.fillRect(50, 50, 350, 300);//cabeza
 
 pincel.fillStyle = "black"
-pincel.fillRect(100, 100, 80,80);
-
-pincel.fillStyle = "black";
-pincel.fillRect (280, 100, 80, 80);
-
-pincel.fillStyle = "black";
-pincel.fillRect(180, 180, 100,140);
-
-pincel.fillStyle = "black"
-pincel.fillRect(120, 220, 60, 130);
-
-pincel.fillStyle = "black"
-pincel.fillRect(280, 220, 60, 130);
+pincel.fillRect(100, 100, 80,80);//hojo izquierdo
+pincel.fillRect (280, 100, 80, 80);//hojo derecho
+pincel.fillRect(180, 180, 100,140);//nariz
+pincel.fillRect(120, 220, 60, 130);//bigote izquierdo
+pincel.fillRect(280, 220, 60, 130);//bigote derecho
