d4aea30a9c4cab72da1373d89d80035c359165f1
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-23 21:52:00 -0600

    Subimos carpeta con archivos

---
+++ Minecraft/index.js
@@ -0,0 +1,21 @@
+
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+
+pincel.fillStyle = "green"
+pincel.fillRect(50, 50, 350, 300);
+
+pincel.fillStyle = "black"
+pincel.fillRect(100, 100, 80,80);
+
+pincel.fillStyle = "black";
+pincel.fillRect (280, 100, 80, 80);
+
+pincel.fillStyle = "black";
+pincel.fillRect(180, 180, 100,140);
+
+pincel.fillStyle = "black"
+pincel.fillRect(120, 220, 60, 130);
+
+pincel.fillStyle = "black"
+pincel.fillRect(280, 220, 60, 130);
