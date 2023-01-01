5eb37cba2418139831b9ac36e1283f2a9688f82a
Emmanuelvp88 <Emmanuel.villalvap@outlook.es>
2022-12-31 23:36:36 -0600

    pusimos el sirculo en la bandera

--- Bandera/index.js
+++ Bandera/index.js
@@ -1,1 +1,13 @@
-alert("dibadiuabsiud");
+let pantalla = document.querySelector("canvas");
+let pincel = pantalla.getContext("2d")
+
+pincel.fillStyle = "green";
+pincel.fillRect(0, 0, 200, 400);
+
+pincel.fillStyle = "red";
+pincel.fillRect(400,0, 200, 400);
+
+pincel.fillStyle= "grey"
+pincel.beginPath();
+pincel.arc(300,200, 95,0, 2*3.1416);
+pincel.fill();
